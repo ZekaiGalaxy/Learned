288bbe7d4a67a0e5a8a3253393b753b11da83a8d
zekai <77915605+violets-blue@users.noreply.github.com>
2023-01-05 20:34:59 +0800

    Create evaluation.py

---
+++ Code_Snippets/evaluation.py
@@ -0,0 +1,15 @@
+use evaluate instead of datasets
+evaluate.load_metric()
+
+# classification
+acc, precision, recall, f1
+GLUE
+
+# QA
+EM
+SQuAD, XNLI
+
+# generation
+rouge, bleu, ppl, bertscore
+bleurt, meteor
+
